8b4850823f1c72e4d840ae668bb282c60b6515fc
j <j>
2023-01-30 19:52:41 -0700

    GameState Script Recreated as PauseGame Script and Reassigned to MainManger Object

--- Scripts/PauseGame.cs
+++ Scripts/PauseGame.cs
@@ -2,29 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameState : MonoBehaviour
+public class PauseGame : MonoBehaviour
 {
     [SerializeField] private MenuManager menuManager;
     [SerializeField] private GameObject pauseMenu;
 
-    private bool inGame;
-    private bool gameOver;
+    private bool paused;
 
 
     private void Update()
     {
         // set game state and time scale, based on the existence of the pauseMenu
-        inGame = !GameObject.Find($"{pauseMenu.name}(Clone)");
-        Time.timeScale = inGame ? 1 : 0;
+        paused = GameObject.Find($"{pauseMenu.name}(Clone)");
+        Time.timeScale = paused ? 0 : 1;
 
 
         // handles the pauseMenu state, based on the game state. When "esc" is pressed
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (inGame)
-                menuManager.OpenMenu(pauseMenu);
-            else
+            if (paused)
                 menuManager.CloseMenu();
+            else
+                menuManager.OpenMenu(pauseMenu);
         }
     }
 }
